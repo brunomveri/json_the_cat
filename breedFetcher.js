a5be7647e70abf159f62f58a38c73ed73c9a9f1a
vagrant <mveri.bruno@gmail.com>
2021-01-18 21:49:15 +0000

    Fixed the error case when fetching for a non-existent/invalid breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,31 @@
-const breed = process.argv[2];
+// const breed = process.argv[2]; //Moved to index.js
 
 const request = require('request');
 
 const url = 'https://api.thecatapi.com/v1/breeds/search';
 
-request(url + '?q=' + breed, (error, response, body) => {
-  if (error) {
-    console.log('Server failed to reponse', error);
-    return error;
-  }
-  const data = JSON.parse(body);
-  const catBreed = data[0];
- if (catBreed) {
-   console.log(catBreed.description);
- } else {
- console.log(`Failed to find ${breed}`);
- }
-});
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(url + '?q=' + breedName, (error, response, body) => {
+    
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    
+    const data = JSON.parse(body);
+    const catBreedDescription = data[0];
+    
+    if (catBreedDescription) {
+      callback(null, catBreedDescription.description);
+    } else {
+      // console.log(`Failed to find ${breedName}`); //FIX THIS!!
+      callback(`Failed to find ${breedName}`, null);
+    }
+
+  });
+
+};
+
+
+module.exports = { fetchBreedDescription };
